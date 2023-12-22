558ebcb3c1ae48a53cf517e68f1b9c245fa37fa3
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-12-22 20:20:25 +0530

    Added language selector to CodeEditor component and updated global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
+import { useState } from "react";
 import CodeEditor from "@/components/CodeEditor";
+import { languages } from "@/utils/utilities";
+import LanguageSelector from "@/components/LanguageSelector";
 
 export default function Home() {
+  const [language, setLanguage] = useState(languages[0].name);
+
   return (
     <main>
+      <header>
+        <LanguageSelector />
+      </header>
       <div className="code-editor-ref">
-        <CodeEditor />
+        <CodeEditor language={language} />
       </div>
     </main>
   );
--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -1,9 +1,62 @@
 "use client";
 
 import React from "react";
+import { Resizable } from "re-resizable";
+import AceEditor from "react-ace";
 
-function CodeEditor() {
-  return <div>CodeEditor</div>;
+// languages for ace editor
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-typescript";
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-css";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-c_cpp";
+
+// themes for ace editor
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-solarized_light";
+import "ace-builds/src-noconflict/theme-solarized_dark";
+import "ace-builds/src-noconflict/theme-terminal";
+import "ace-builds/src-noconflict/theme-twilight";
+
+interface CodeEditorProps {
+  onCodeChange: (code: string) => void;
+  language: string;
+  theme: string;
+  icon: string;
+  background?: string;
+  currentPadding?: string;
+}
+
+function CodeEditor({
+  onCodeChange,
+  language,
+  theme,
+  icon,
+  background,
+  currentPadding,
+}: CodeEditorProps) {
+  return (
+    <Resizable minHeight={466} minWidth={510} maxWidth={1000}>
+      <div>
+        <AceEditor
+          value="function() { return 'Hello World'; }"
+          name="UNIQUE_ID_OF_DIV"
+          fontSize={16}
+          theme="monokai"
+          mode={language.toLowerCase()}
+          wrapEnabled={true}
+          showGutter={false}
+          showPrintMargin={false}
+          highlightActiveLine={false}
+          editorProps={{ $blockScrolling: true }}
+          className="ace-editor-container"
+        />
+      </div>
+    </Resizable>
+  );
 }
 
 export default CodeEditor;
