8f648e535370f3db31d3a5151c1603041ad5533d
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-12-22 22:07:35 +0530

    Update: Code editor styling and add language icon

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Resizable } from "re-resizable";
 import AceEditor from "react-ace";
 
@@ -20,6 +20,7 @@ import "ace-builds/src-noconflict/theme-solarized_light";
 import "ace-builds/src-noconflict/theme-solarized_dark";
 import "ace-builds/src-noconflict/theme-terminal";
 import "ace-builds/src-noconflict/theme-twilight";
+import Image from "next/image";
 
 interface CodeEditorProps {
   onCodeChange: (code: string) => void;
@@ -38,9 +39,59 @@ function CodeEditor({
   background,
   currentPadding,
 }: CodeEditorProps) {
+  const [width, setWidth] = useState(1000);
+  const [height, setHeight] = useState<number | null>(500);
+
+  // @ts-ignore
+  const handleResize = (event, direction, ref, pos) => {
+    const newHeight = ref.style.height;
+    setHeight(parseInt(newHeight));
+  };
+
+  const updateSize = () => {
+    setHeight(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
   return (
-    <Resizable minHeight={466} minWidth={510} maxWidth={1000}>
-      <div>
+    <Resizable
+      minHeight={466}
+      minWidth={510}
+      maxWidth={1000}
+      defaultSize={{
+        width: width,
+        height: height || 500,
+      }}
+      onResize={handleResize}
+      className="resize-container relative"
+    >
+      <div className="code-block">
+        <div className="code-title h-[52px] px-4 flex items-center justify-between bg-black bg-opacity-80">
+          {/* three dots */}
+          <div className="dots flex items-center gap-1">
+            <div className="w-3 h-3 rounded-full bg-[#ff5656]"></div>
+            <div className="w-3 h-3 rounded-full bg-[#ffbc6a]"></div>
+            <div className="w-3 h-3 rounded-full bg-[#67f772]"></div>
+          </div>
+
+          {/* input */}
+          <div className="input-control w-full">
+            <input
+              type="text"
+              className="w-full text-[hsla(0,0%,100%,.6)] outline-none font-medium text-center bg-transparent"
+            />
+          </div>
+
+          {/* language icon */}
+          <div className="icon flex justify-center items-center p-1 bg-black bg-opacity-30 rounded-sm ">
+            <Image src={icon} alt="language icon" width={20} height={20} />
+          </div>
+        </div>
         <AceEditor
           value="function() { return 'Hello World'; }"
           name="UNIQUE_ID_OF_DIV"
