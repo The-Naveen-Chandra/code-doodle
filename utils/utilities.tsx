759f01cfb414968841e203c614fea9f2a9a09090
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-12-22 20:15:47 +0530

    Dev: Utility module with lang. module

---
+++ utils/utilities.tsx
@@ -0,0 +1,9 @@
+export const languages = [
+  { name: "javascript", icon: "icons/javascript.svg" },
+  { name: "typescript", icon: "icons/typescript.svg" },
+  { name: "html", icon: "icons/html.svg" },
+  { name: "css", icon: "icons/css.svg" },
+  { name: "python", icon: "icons/python.svg" },
+  { name: "java", icon: "icons/java.svg" },
+  { name: "c_cpp", icon: "icons/c_cpp.svg" },
+];
